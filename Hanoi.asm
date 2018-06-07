d153d24d0af296de56e3a23315b6c07033aae9ca
Omar Madriz <madriz_omar@hotmail.com>
2018-06-07 13:31:34 -0500

    Creacion de las torres.

--- Hanoi.asm
+++ Hanoi.asm
@@ -1,3 +1,10 @@
 # Nombres: Jose Luis Torrentera Arroniz; Omar Antonio Madriz Almanza
 # Fecha: 07 de Junio de 2018
 # Descripción: Torres de Hanoi
+
+.data
+	TorreA: .word 1 2 3
+	TorreB: .word 0 0 0
+	TorreC: .word 0 0 0
+
+.text
